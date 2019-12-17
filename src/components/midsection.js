8a55756fac05bd1e351a235b55d842118c72429b
Trevor Johnson <trevorjohnson@Trevors-PWC-MBP.fios-router.home>
2019-12-16 23:55:30 -0500

    update videos

--- src/components/midsection.js
+++ src/components/midsection.js
@@ -7,11 +7,11 @@ const Midsection = () => (
 	<div>
 		<section className="section">
 			<div className="container">
-				<p className="title is-1 has-text-weight-bold">Which Job is Right for You?</p>
+				<p className="title is-2 has-text-weight-bold">Which Job is Right for You?</p>
 				<p className="subtitle is-5">It takes hundreds of smiling faces with many different talents to deliver on our promise of fun and safety every summer. Select a job category to learn more about our opportunities.</p>
 				<div className="columns is-multiline">
 					<div className="column is-half">
-						<div className="notification">
+						<div className="notification is-success">
 							<h1 className="title is-size-4">Lifeguards</h1>
 									<p className="subtitle is-size-5">
 									Are you ready to join an elite team? Our lifeguarding staff is among the nation's most decorated and successful. We are looking for talented new team members to join us this summer.
@@ -20,7 +20,7 @@ const Midsection = () => (
 						</div>
 					</div>
 					<div className="column is-half">
-						<div className="notification">
+						<div className="notification is-success">
 							<h1 className="title is-size-4">Counselors</h1>
 									<p className="subtitle is-size-5">
 									Do you enjoy the outdoors, sports and crafts? Do you have a passion for working with youth and helping them reach their full potential? If so, we've got the ultimate summer job for you!
@@ -29,7 +29,7 @@ const Midsection = () => (
 						</div>
 					</div>
 					<div className="column is-half">
-						<div className="notification">
+						<div className="notification is-success">
 							<h1 className="title is-size-4">Concessions</h1>
 									<p className="subtitle is-size-5">
 									Do you enjoy the excitement of a fast-paced kitchen? Want to ensure guests are served safe, delicious food during their visits? This might be the right job for you! Click to learn more...
@@ -38,7 +38,7 @@ const Midsection = () => (
 						</div>
 					</div>
 					<div className="column is-half">
-						<div className="notification">
+						<div className="notification is-success">
 							<h1 className="title is-size-4">Guest Services</h1>
 									<p className="subtitle is-size-5">
 									It takes a huge seasonal staff to deliver excellent customer service in our parks every summer. We're looking for motivated people with smiling faces. Do you have what it takes?
@@ -52,7 +52,7 @@ const Midsection = () => (
 
 		<section className="section">
 			<div className="container">
-				<p className="title is-1 has-text-weight-bold">Attend one of our Hiring Events</p>
+				<p className="title is-2 has-text-weight-bold">Attend one of our Hiring Events</p>
 				<p className="subtitle is-5">Our hiring managers will be holding open interviews at each of the events listed. Lifeguard applicants will take swim tests when attending lifeguard-specific hiring events, so bring your swimsuit to the hiring event!</p>
 				<div className="columns is-multiline">
 					<div className="column is-one-third-desktop is-half-tablet">
@@ -111,11 +111,15 @@ const Midsection = () => (
 		<section className="section">
 			<div className="container">
 				<div className="columns is-multiline">
-					<div className="column is-half">
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/i_Y0Uhog-bE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<div className="column is-half video-responsive">
+						<div className="video-responsive">
+							<iframe width="100%" height="100%" src="https://www.youtube.com/embed/i_Y0Uhog-bE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
 					</div>
 					<div className="column is-half">
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/w1oDlCCwOgM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<div className="video-responsive">
+							<iframe width="100%" height="100%" src="https://www.youtube.com/embed/w1oDlCCwOgM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
 					</div>
 				</div>
 			</div>
